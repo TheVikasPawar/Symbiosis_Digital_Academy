0d4a36e04d426e1f5c411258770065817524b7fe
Vikas Pawar <98597814+TheVikasPawar@users.noreply.github.com>
2023-09-27 21:17:34 +0530

    Add files via upload

---
+++ Day5/InstanceOf.java
@@ -0,0 +1,9 @@
+package Day5;
+
+public class InstanceOf {
+	public static void main(String[] args) {
+		InstanceOf s = new InstanceOf();
+		System.out.println(s instanceof InstanceOf);
+	}
+
+}
---
+++ Day5/Student6.java
@@ -0,0 +1,33 @@
+package Day5;
+
+// Java Program to initialize the values 
+// of one object to another object.
+
+public class Student6 {
+	int id;
+	String name;
+
+	Student6(int i, String n) {
+		id = i;
+		name = n;
+	}
+
+	Student6(Student6 s) {
+		id = s.id;
+		name = s.name;
+	}
+
+	void display() {
+		System.out.println(id + " " + name);
+	}
+
+	public static void main(String[] args) {
+		Student6 s1 = new Student6(12,"ABC");
+		Student6 s2 = new Student6(s1);
+		
+		s1.display();
+		s2.display();
+
+	}
+
+}
