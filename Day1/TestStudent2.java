acaf722dc1bf709f4820254d8cd613daf8602b4e
Vikas Pawar <98597814+TheVikasPawar@users.noreply.github.com>
2023-09-27 07:47:01 +0530

    Add files via upload

---
+++ Day1/TestStudent2.java
@@ -0,0 +1,17 @@
+package Day1;
+
+class Student2{
+	int id;
+	String name;
+}
+public class TestStudent2 {
+	public static void main(String[] args) {
+		Student2 s1=new Student2();
+		
+		s1.id=456;
+		s1.name="Vikas";
+		
+		System.out.println(s1.id+" "+s1.name);
+	}
+
+}
