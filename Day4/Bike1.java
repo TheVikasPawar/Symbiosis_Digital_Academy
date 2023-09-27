9c803863577f8b0f7cb2bd6d96e9e382b737a709
Vikas Pawar <98597814+TheVikasPawar@users.noreply.github.com>
2023-09-27 07:42:56 +0530

    Add files via upload

---
+++ Day4/Bike1.java
@@ -0,0 +1,15 @@
+package Day4;
+
+// Default Constructor
+
+public class Bike1 {
+	Bike1() {
+		System.out.println("Bike is Created...!");
+	}
+
+	public static void main(String[] args) {
+		Bike1 ref = new Bike1();
+
+	}
+
+}
